e7540369f19f7d649f1a85593fd929069f1d81c5
rattboi <rattboi@gmail.com>
2013-11-13 23:18:26 -0800

    Fixing installer a bit more

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash 
 
 PWD=$(pwd)
-EASYFILES="oh-my-zsh zshrc vimrc tmux.conf pentadactylrc Xdefaults"
+EASYFILES="oh-my-zsh zshrc vimrc pentadactylrc Xdefaults tmux.conf"
 
 # get submodules set up
 git submodule init
