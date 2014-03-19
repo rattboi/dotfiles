9b38fa39a1d7a3c8d541693be93db6422170d2e4
Bradon Kanyid <rattboi@gmail.com>
2014-03-19 13:58:27 -0700

    Added gitv log viewer plugin, ddg as default search in ff, retab of emacs init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -3,24 +3,24 @@
 ;; Install el-get 
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 (unless (require 'el-get nil 'noerror)
-    (with-current-buffer
-        (url-retrieve-synchronously
-             "http://raw.github.com/dimitri/el-get/master/el-get-install.el")
-            (let (el-get-master-branch)
-                 (goto-char (point-max))
-                 (eval-print-last-sexp))))
+  (with-current-buffer
+    (url-retrieve-synchronously
+      "http://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (let (el-get-master-branch)
+      (goto-char (point-max))
+      (eval-print-last-sexp))))
 
 (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
 
 ;; Install packages
- (add-to-list 'el-get-sources
-   '(:name evil          :type elpa   :pkgname "evil")
-   '(:name evil-leader   :type elpa   :pkgname "evil-leader"))
+(add-to-list 'el-get-sources
+             '(:name evil          :type elpa   :pkgname "evil")
+             '(:name evil-leader   :type elpa   :pkgname "evil-leader"))
 
 (setq my-el-get-packages  
-    (append  
-     '(evil evil-leader evil-org cyberpunk-theme)  
-      (mapcar 'el-get-source-name el-get-sources)))  
+      (append  
+        '(evil evil-leader evil-org cyberpunk-theme)  
+        (mapcar 'el-get-source-name el-get-sources)))  
 
 (el-get 'sync my-el-get-packages)
 
@@ -30,7 +30,7 @@
 ;; disable some of the crap on startup
 (setq inhibit-startup-message t)
 (if (display-graphic-p)
-    (tool-bar-mode -1))
+  (tool-bar-mode -1))
 
 (require 'evil-leader)
 (require 'evil)
